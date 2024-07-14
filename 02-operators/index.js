aa591ddf788f8b743e2193ad593e53e8471a1a34
MRINMOY MONDAL <mm2922894@gmail.com>
2024-07-14 11:40:16 +0530

    completed day 2 but feature request is pending

---
+++ 02-operators/index.js
@@ -0,0 +1,87 @@
+//NOTE: Activity 1
+//NOTE: Task 1
+const numOne = 45;
+const numTwo = 18;
+console.log(numOne + numTwo);
+
+//NOTE: Task 2
+const numThree = 17;
+const numFour = 7;
+console.log(numThree - numFour);
+
+//NOTE: Task 3
+const numFive = 7;
+const numSix = 18;
+console.log(numFive * numSix);
+
+//NOTE: Task 4
+const numSeven = 11;
+const numEight = 12;
+console.log(numSeven / numEight);
+
+//NOTE: Task 5
+const divident = 12;
+const divisor = 7;
+const remainder = 12 % 7;
+console.log(remainder);
+
+//NOTE: Activity 2
+//NOTE: Task 6
+let initNum = 17;
+const newNum = (initNum += 26);
+console.log(newNum);
+
+//NOTE: Task 7
+let initNum2 = 17;
+const newNum2 = (initNum2 -= 26);
+console.log(newNum2);
+
+//NOTE: Activity 3
+//NOTE: Task 8
+const valOne = 4;
+const valTwo = 9;
+const valThree = 3;
+const comVal = valOne < valTwo && valTwo > valThree;
+console.log(comVal);
+
+//NOTE: Task 9
+const valOne2 = 4;
+const valTwo2 = 9;
+const valThree2 = 34;
+const comVal2 = valOne2 <= valTwo2 && valTwo2 >= valThree2;
+console.log(comVal2);
+
+//NOTE: Task 10
+const valueOne = 4;
+const valueTwo = 9;
+const valueThree = 9;
+const doubleEqual = valueOne == valueTwo;
+const tripleEqual = valueTwo === valueThree;
+console.log(doubleEqual, tripleEqual);
+
+//NOTE: Activity 4
+//NOTE: Task 11
+const valOne3 = 4;
+const valTwo3 = 9;
+const valThree3 = 34;
+const comVal3 = valOne3 <= valTwo3 && valTwo3 >= valThree3;
+console.log(comVal3);
+
+//NOTE: Task 12
+const valOne4 = 4;
+const valTwo4 = 9;
+const valThree4 = 34;
+const comVal4 = valOne4 <= valTwo4 || valTwo4 >= valThree4;
+console.log(comVal4);
+
+//NOTE: Task 13
+const valueOne2 = 4;
+const valueTwo2 = 9;
+const doubleEqual2 = valueOne2 == valueTwo2;
+console.log(!doubleEqual2);
+
+//NOTE: Task 14
+const valueOne3 = 4;
+const valueTwo3 = 9;
+const doubleEqual3 = valueOne3 == valueTwo3;
+console.log(valueOne3 === true ? true : false);
